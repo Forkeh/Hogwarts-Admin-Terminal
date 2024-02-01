94a71b0172a016496dad15bc857184c6c83fb28a
Forkeh <forkeh@gmail.com>
2024-02-01 21:24:19 +0100

    initial commit

---
+++ src/edu/hogwarts/Person.java
@@ -0,0 +1,55 @@
+package edu.hogwarts;
+
+public class Person {
+    private String lastName;
+    private String firstName;
+    private String middleName;
+
+
+
+    public Person() {
+        this.lastName = "lastName";
+        this.firstName = "firstName";
+        this.middleName = "middleName";
+    }
+
+    public Person(String lastName, String firstName, String middleName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                '}';
+    }
+}
